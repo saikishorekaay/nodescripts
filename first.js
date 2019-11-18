d6cfb0528ee4fb4de0dcd3b49baee3b6cf9cd052
saikishorekaay <saikishore.rajendran@gmail.com>
2019-11-18 18:35:54 +0530

    edit aws ecs

--- first.js
+++ first.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('this is my test js!');
+  res.end('this is my test ecs file!');
 }).listen(8080);
