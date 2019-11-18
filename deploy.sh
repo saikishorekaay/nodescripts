a18fffea62e7f26f2e5e566b8439828b5f6e30ba
saikishorekaay <saikishore.rajendran@gmail.com>
2019-11-18 18:49:12 +0530

    edit aws ecs2

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ aws ecs register-task-definition --family node-app --cli-input-json file://node-
 TASK_REVISION=`aws ecs describe-task-definition --task-definition node-app --region ap-south-1 | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --region ap-south-1 | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 echo $DESIRED_COUNT
-if [ $DESIRED_COUNT == "0" ]; then
-    DESIRED_COUNT="1"
+if [ $DESIRED_COUNT -eq 0 ]; then
+    DESIRED_COUNT=1
 fi
 
 aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT} --region ap-south-1
