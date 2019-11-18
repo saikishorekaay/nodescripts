871c2ac58bfe96b18dcc0c1de7f721f64de4ca1c
saikishorekaay <saikishore.rajendran@gmail.com>
2019-11-18 19:08:00 +0530

    edit aws ecs4

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 SERVICE_NAME="node-app"
 IMAGE_VERSION="v${BUILD_NUMBER}"
+echo $IMAGE_VERSION
 TASK_FAMILY="node-app"
 
 # Create a new task definition for this build
-sed -e "s;%CURRENT_BUILD%;${IMAGE_VERSION};g" node.json > node-v_${BUILD_NUMBER}.json
+sed -e "s;CURRENT_BUILD;${IMAGE_VERSION};g" node.json > node-v_${BUILD_NUMBER}.json
 aws ecs register-task-definition --family node-app --cli-input-json file://node-v_${BUILD_NUMBER}.json --region ap-south-1
 
 # Update the service with the new task definition and desired count
 TASK_REVISION=`aws ecs describe-task-definition --task-definition node-app --region ap-south-1 | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
-DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --region ap-south-1 | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+DESIRED_COUNT=`aws ecs describe-services --cluster nodeapp --services ${SERVICE_NAME} --region ap-south-1 | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 echo $DESIRED_COUNT
 # if [ $DESIRED_COUNT -eq 0 ]; then
 #     DESIRED_COUNT=1
